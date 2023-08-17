1c4caed1b51bcb49c40f2bfbc88a2557e307dd3d
Choijo-gunhee <abcdekun@naver.com>
2023-08-18 00:47:42 +0900

    Connect myReview page & likeReview page with axios

--- src/pages/MyPage/LikeReview.jsx
+++ src/pages/MyPage/LikeReview.jsx
@@ -4,13 +4,17 @@ import { IoPerson } from 'react-icons/io5';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { FaHeart } from 'react-icons/fa';
 // import { motion } from 'framer-motion';
-import { Reviews } from '../../data/Reviews';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { LoginStateAtom } from '../../atom';
 
 const Container = styled.div`
   display: flex;
   width: 100%;
   flex-direction: column;
   gap: 4rem;
+  min-height: 100vh;
 `;
 const ContentRow = styled.div`
   display: flex;
@@ -30,9 +34,7 @@ const ReviewRow = styled.div`
 const ReviewItem = styled.div`
   display: flex;
   flex-direction: column;
-  width: calc(
-    (100% - 3.6rem) / 4
-  ); // 현재 한 행에 4개의 리뷰를 보여줍니다. 필요하다면 이 값을 조절할 수 있습니다.
+  width: calc((100% - 3.6rem) / 4);
   gap: 0.5rem;
 `;
 const ReviewThumbnailContainer = styled.div`
@@ -74,14 +76,14 @@ const ReviewUser = styled.div`
 const ReviewRate = styled.div`
   color: #f90;
 `;
-const ReviewDesc = styled.p`
-  font-size: 0.8rem;
-  line-height: 1.3;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
-`;
+// const ReviewDesc = styled.p`
+//   font-size: 0.8rem;
+//   line-height: 1.3;
+//   display: -webkit-box;
+//   -webkit-box-orient: vertical;
+//   -webkit-line-clamp: 3;
+//   overflow: hidden;
+// `;
 const ReviewItemFooter = styled.div`
   display: flex;
   justify-content: space-between;
@@ -100,8 +102,27 @@ const ReviewLikes = styled.span`
     font-size: 1rem;
   }
 `;
-
 const LikeReview = () => {
+  const [likeReviews, setLikeReviews] = useState([]);
+  const { accessToken } = useRecoilValue(LoginStateAtom);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('백엔드api 주소 파트', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        setLikeReviews(response.data.content);
+      } catch (error) {
+        console.error('응 에러야~ 다시해~', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Container>
       <h1
@@ -114,65 +135,49 @@ const LikeReview = () => {
         공감한 리뷰
       </h1>
       <ContentRow>
-        <ReviewRow>
-          {Reviews.map((item, index) => (
-            <ReviewItem key={index}>
-              <ReviewThumbnailContainer>
-                <ReviewThumbnail src={item.thumbnail} />
-                <ReviewVideoTitle>{item.videoTitle}</ReviewVideoTitle>
-              </ReviewThumbnailContainer>
-              <ReviewUserContainer>
-                <ReviewUser>
-                  <IoPerson />
-                  {item.userName}
-                </ReviewUser>
-                <ReviewRate>
-                  {(function () {
-                    const stars = [];
-                    for (let i = 0; i < item.rate; i++) {
-                      stars.push(<AiFillStar key={'filled' + i} />);
-                    }
-                    for (let i = 0; i < 5 - item.rate; i++) {
-                      stars.push(<AiOutlineStar key={'outlined' + i} />);
-                    }
-                    return stars;
-                  })()}
-                </ReviewRate>
-              </ReviewUserContainer>
-              <ReviewDesc>{item.desc}</ReviewDesc>
-              <ReviewItemFooter>
-                <ReviewDate>{item.date}</ReviewDate>
-                <ReviewLikes>
-                  <FaHeart />
-                  {item.likes}
-                </ReviewLikes>
-              </ReviewItemFooter>
-            </ReviewItem>
-          ))}
-        </ReviewRow>
+        {likeReviews.length === 0 ? (
+          <p>좋아요를 누른 리뷰가 없어요</p>
+        ) : (
+          <ReviewRow>
+            {likeReviews.map((item, index) => (
+              <ReviewItem key={index}>
+                <ReviewThumbnailContainer>
+                  <ReviewThumbnail src={item.linkURL} />
+                  <ReviewVideoTitle>{item.title}</ReviewVideoTitle>
+                </ReviewThumbnailContainer>
+                <ReviewUserContainer>
+                  <ReviewUser>
+                    <IoPerson />
+                    {item.writer}
+                  </ReviewUser>
+                  <ReviewRate>
+                    {(function () {
+                      const stars = [];
+                      for (let i = 0; i < item.rating; i++) {
+                        stars.push(<AiFillStar key={'filled' + i} />);
+                      }
+                      for (let i = 0; i < 5 - item.rating; i++) {
+                        stars.push(<AiOutlineStar key={'outlined' + i} />);
+                      }
+                      return stars;
+                    })()}
+                  </ReviewRate>
+                </ReviewUserContainer>
+                {/* <ReviewDesc>{item.desc}</ReviewDesc> */}
+                <ReviewItemFooter>
+                  <ReviewDate>{item.reviewCreatedTime}</ReviewDate>
+                  <ReviewLikes>
+                    <FaHeart />
+                    {item.likes}
+                  </ReviewLikes>
+                </ReviewItemFooter>
+              </ReviewItem>
+            ))}
+          </ReviewRow>
+        )}
       </ContentRow>
     </Container>
   );
 };
 
 export default LikeReview;
-
-// const Reviews = [
-//   {
-//     thumbnail:
-//       "https://i.ytimg.com/vi/J9DlrnNlgfE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDIl-EjUPHzEQNfXBWWG-r9GjHWCw",
-//     videoTitle:
-//       "NewJeans(뉴진스) - Super Shy @인기가요 inkigayo 20230716 게시자: 스브스케이팝 X INKIGAYO",
-//     userName: "뉴진스짱",
-//     rate: 4,
-//     desc: "뉴진스도 뉴진스인데 코디랑 헤메가 미쳤다 ..곡이랑 너무 잘어울리고 멤버별로 특색입게 입히면서 통일성 있음. 올드하지 않으면서 세련되게 잘 입히는 듯.",
-//     date: "2023.07.27",
-//     likes: 146,
-//   },
-// ];
-
-// ... (스타일링 코드는 동일하게 유지합니다.)
-
-// ... (다른 import 문은 그대로 유지)
-
-// ... (나머지 스타일과 컴포넌트 코드는 그대로 유지)
--- src/pages/MyPage/MyWriting.jsx
+++ src/pages/MyPage/MyWriting.jsx
@@ -1,19 +1,43 @@
 // import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MyPageNav from './MyPageNav';
 import { Outlet, useRoutes } from 'react-router-dom';
 import styled from 'styled-components';
 import { IoPerson } from 'react-icons/io5';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { FaHeart } from 'react-icons/fa';
-import { Reviews } from '../../data/Reviews';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { LoginStateAtom } from '../../atom';
 
 function MyWriting() {
+  const { accessToken } = useRecoilValue(LoginStateAtom);
+  const [myReviews, setMyReviews] = useState([]);
   const hasChildRoutes = useRoutes([
     { path: 'account', element: <></> },
     { path: 'likereview', element: <></> },
     { path: 'category', element: <></> },
   ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://port-0-tubenova-backend-eu1k2llldkkxjy.sel3.cloudtype.app/reviews/my',
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        setMyReviews(response.data.content);
+      } catch (error) {
+        console.error('응 에러야~ 다시해봐~', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -34,45 +58,50 @@ function MyWriting() {
               >
                 내가 올린 리뷰
               </h1>
+
               <ContentRow>
-                <ReviewRow>
-                  {Reviews.map((item, index) => (
-                    <ReviewItem key={index}>
-                      <ReviewThumbnailContainer>
-                        <ReviewThumbnail src={item.thumbnail} />
-                        <ReviewVideoTitle>{item.videoTitle}</ReviewVideoTitle>
-                      </ReviewThumbnailContainer>
-                      <ReviewUserContainer>
-                        <ReviewUser>
-                          <IoPerson />
-                          {item.userName}
-                        </ReviewUser>
-                        <ReviewRate>
-                          {(function () {
-                            const stars = [];
-                            for (let i = 0; i < item.rate; i++) {
-                              stars.push(<AiFillStar key={'filled' + i} />);
-                            }
-                            for (let i = 0; i < 5 - item.rate; i++) {
-                              stars.push(
-                                <AiOutlineStar key={'outlined' + i} />
-                              );
-                            }
-                            return stars;
-                          })()}
-                        </ReviewRate>
-                      </ReviewUserContainer>
-                      <ReviewDesc>{item.desc}</ReviewDesc>
-                      <ReviewItemFooter>
-                        <ReviewDate>{item.date}</ReviewDate>
-                        <ReviewLikes>
-                          <FaHeart />
-                          {item.likes}
-                        </ReviewLikes>
-                      </ReviewItemFooter>
-                    </ReviewItem>
-                  ))}
-                </ReviewRow>
+                {myReviews.length === 0 ? (
+                  <p>내가 올린 리뷰가 없어요</p>
+                ) : (
+                  <ReviewRow>
+                    {myReviews.map((item, index) => (
+                      <ReviewItem key={index}>
+                        <ReviewThumbnailContainer>
+                          <ReviewThumbnail src={item.linkURL} />
+                          <ReviewVideoTitle>{item.title}</ReviewVideoTitle>
+                        </ReviewThumbnailContainer>
+                        <ReviewUserContainer>
+                          <ReviewUser>
+                            <IoPerson />
+                            {item.writer}
+                          </ReviewUser>
+                          <ReviewRate>
+                            {(function () {
+                              const stars = [];
+                              for (let i = 0; i < item.rating; i++) {
+                                stars.push(<AiFillStar key={'filled' + i} />);
+                              }
+                              for (let i = 0; i < 5 - item.rating; i++) {
+                                stars.push(
+                                  <AiOutlineStar key={'outlined' + i} />
+                                );
+                              }
+                              return stars;
+                            })()}
+                          </ReviewRate>
+                        </ReviewUserContainer>
+                        {/* <ReviewDesc>{item.desc}</ReviewDesc> */}
+                        <ReviewItemFooter>
+                          <ReviewDate>{item.reviewCreatedTime}</ReviewDate>
+                          <ReviewLikes>
+                            <FaHeart />
+                            {item.likes}
+                          </ReviewLikes>
+                        </ReviewItemFooter>
+                      </ReviewItem>
+                    ))}
+                  </ReviewRow>
+                )}
               </ContentRow>
             </Container>
           </>
@@ -89,6 +118,7 @@ const MyPageContent = styled.div`
 const Container = styled.div`
   display: flex;
   width: 100%;
+  min-height: 100vh;
   flex-direction: column;
   gap: 4rem;
 `;
@@ -154,14 +184,14 @@ const ReviewUser = styled.div`
 const ReviewRate = styled.div`
   color: #f90;
 `;
-const ReviewDesc = styled.p`
-  font-size: 0.8rem;
-  line-height: 1.3;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
-`;
+// const ReviewDesc = styled.p`
+//   font-size: 0.8rem;
+//   line-height: 1.3;
+//   display: -webkit-box;
+//   -webkit-box-orient: vertical;
+//   -webkit-line-clamp: 3;
+//   overflow: hidden;
+// `;
 const ReviewItemFooter = styled.div`
   display: flex;
   justify-content: space-between;
