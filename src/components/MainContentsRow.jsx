09947318b81656ed220fe0c4349787dbce31e53b
gaeunnlee <gaeunnnleee@gmail.com>
2023-08-17 21:16:14 +0900

    feat: side nav responsive

--- src/components/MainContentsRow.jsx
+++ src/components/MainContentsRow.jsx
@@ -8,6 +8,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { Reviews } from "../data/Reviews";
 import emptyImage from "../static/empty.png";
+import { useWindowSize } from "../hook/useWindow";
 
 const Container = styled.div`
   display: flex;
@@ -390,28 +391,4 @@ export default function MainContentsRow({ contentsTitle, userLikes }) {
     </Container>
   );
 }
-function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-  return windowSize;
-}
+
